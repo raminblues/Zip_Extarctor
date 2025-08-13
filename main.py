bbb3701551cac1556556e728cdeb8e999ef2b666
ramin <raminblues2003@gmail.com>
2025-08-13 20:47:50 +0330

    A zip extractor app

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import FreeSimpleGUI as sg
+from zip_extractor import extract_archive
 
 sg.theme("black")
 
@@ -8,7 +9,7 @@ choose_button1 = sg.FileBrowse("Choose", key="archive")
 
 label2 = sg.Text("Select destination directory:")
 input2 = sg.Input()
-choose_button2 = sg.FileBrowse("Choose", key="folder")
+choose_button2 = sg.FolderBrowse("Choose", key="folder")
 
 extract_button = sg.Button("Extract")
 output_label = sg.Text(key="output", text_color="green")
@@ -18,6 +19,12 @@ window = sg.Window("Archive Extractor",
                            [label2, input2, choose_button2],
                            [extract_button, output_label]])
 
+while True:
+    event, values = window.read()
+    print(event, values)
+    archivepath = values["archive"]
+    dest_dir = values["folder"]
+    extract_archive(archivepath, dest_dir)
+    window["output"].update(value="Extraction Completed")
 
-window.read()
 window.close()
